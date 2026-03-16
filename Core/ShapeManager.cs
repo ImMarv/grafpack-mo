e2a4a4b2007e250a9908243eb8fc1dca2cbc228d
Marv O <94709651+ImMarv@users.noreply.github.com>
2026-03-16 16:46:43 +0000

    shape manager - to remove canvas dependency

---
+++ Core/ShapeManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using grafpack_2202368.Shapes;
+
+namespace grafpack_2202368.Core
+{
+    public class ShapeManager
+    {
+        public List<Shape> Shapes = new List<Shape>();
+
+        public Shape GetSelectedShape()
+        {
+            foreach (Shape s in Shapes)
+                if (s.IsSelected)
+                    return s;
+
+            return null;
+        }
+
+        public void DeleteSelectedShape()
+        {
+            Shape s = GetSelectedShape();
+            if (s != null)
+                Shapes.Remove(s);
+        }
+    }
+}
